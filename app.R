f93a7cf9378b7bf45021ea116abf55c2c0008013
Ronin141 <roninvaria@gmail.com>
2018-11-20 00:11:53 +0700

    New line graph

--- app.R
+++ app.R
@@ -1,5 +1,8 @@
 library(shiny)
-library(leaflet)
+library(ggmap)
+library(maptools)
+library(maps)
+# library(leaflet)
 
 r_colors <- rgb(t(col2rgb(colors()) / 255))
 names(r_colors) <- colors()
@@ -11,17 +14,31 @@ ui <- fluidPage(
 
 server <- function(input, output, session) {
   
-  points <- eventReactive(input$recalc, {
-    cbind(statistic_data_with_country$Latitude,statistic_data_with_country$Longitude)
-  }, ignoreNULL = FALSE)
+  # points <- eventReactive(input$recalc, {
+  #   cbind(statistic_data_with_country$Latitude,statistic_data_with_country$Longitude)
+  # }, ignoreNULL = FALSE)
+  # 
+  # output$mymap <- renderLeaflet({
+  #   leaflet() %>%
+  #     addProviderTiles(providers$Stamen.TonerLite,
+  #                      options = providerTileOptions(noWrap = TRUE)
+  #     ) %>%
+  #     addMarkers(data = points())
+  # })
+  visited <- c(statistic_data_with_country$`Country of Nationality`)
+  ll.visited <- geocode(visited)
+  visit.x <- ll.visited$lon
+  visit.y <- ll.visited$lat
+  map("world", fill=TRUE, col="white", bg="lightblue", ylim=c(-60, 90), mar=c(0,0,0,0))
+  points(visit.x,visit.y, col="red", pch=16)
   
-  output$mymap <- renderLeaflet({
-    leaflet() %>%
-      addProviderTiles(providers$Stamen.TonerLite,
-                       options = providerTileOptions(noWrap = TRUE)
-      ) %>%
-      addMarkers(data = points())
-  })
+  # mp <- NULL
+  # mapWorld <- borders("world", colour="gray50", fill="gray50") # create a layer of borders
+  # mp <- ggplot() +   mapWorld
+  # 
+  # #Now Layer the cities on top
+  # mp <- mp+ geom_point(aes(x=visit.x, y=visit.y) ,color="blue", size=3) 
+  # mp
 }
 
 shinyApp(ui, server)
---
+++ new_line.R
@@ -0,0 +1,78 @@
+library(shiny)
+
+dat <- read.csv("line_data.csv")
+year <- dat$years
+
+ui <- fluidPage(
+  
+  titlePanel("Government spending and GDP growth"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      
+      radioButtons("radio", label = h3("Select the sample of countries"),
+                   choices = list("EastAsia", "Europe",
+                                  "TheAmericas","SouthAsia","Oceania","MiddleEast","Africa"),selected = "World"),
+      
+      selectInput("vertical","Show vertical line in year(s):", 
+                  choices = unique(dat$years),multiple=TRUE
+      ),
+      
+      checkboxInput("hor", "Show horizontal axis", TRUE)
+      
+    ),
+    
+    mainPanel(
+      #    h2("main panel"),    
+      #    textOutput("text1"),
+      plotOutput("lineChart")
+    )   
+  )      
+)
+
+server <- function(input, output) {
+  
+  #  output$text1 <- renderText({ 
+  #    paste("The chart is showing fiscal stimulus and real economy growthYou have selected ", input$radio)
+  #  })
+  
+  output$lineChart <- renderPlot({  
+    chartData <- switch(input$radio,
+                        "EastAsia" = list(dat$gr_EastAsia,dat$re_EastAsia),
+                        "Europe" = list(dat$gr_Europe,dat$re_Europe),
+                        "TheAmericas" = list(dat$gr_TheAmericas,dat$re_TheAmericas),
+                        "SouthAsia" = list(dat$gr_SouthAsia,dat$re_SouthAsia) ,
+                        "Oceania" = list(dat$gr_Oceania,dat$re_Oceania),
+                        "MiddleEast" = list(dat$gr_MiddleEast,dat$re_MiddleEast) ,
+                        "Africa" = list(dat$gr_Africa,dat$re_Africa)
+    )  
+    
+    chartTitle <- switch(input$radio,
+                         "EastAsia" = "EastAsia",
+                         "Europe" = "Europe",
+                         "TheAmericas" = "TheAmericas",
+                         "SouthAsia" = "SouthAsia",
+                         "Oceania"="Oceania",
+                         "MiddleEast" = "MiddleEast",
+                         "Africa"="Africa" 
+    )
+    
+    yrange <- c(0,300000)
+    xrange <- range(year)
+    plot(xrange,yrange,type="n",xlab="",ylab="Growth rate (percent)",cex.lab=1.5,
+         main=paste("GDP-weighted averages shown for", chartTitle),
+         sub=c("Data: IMF WEO (10/2015). Chart J. Zilinsky \n Note: Data for 2016 are IMF projections"))
+    lines(year,chartData[[1]],col="aquamarine4",lwd=3)
+    lines(year[1:6],na.omit(chartData[[2]]),col="firebrick3",lwd=3)
+    abline(v=input$vertical,lty=2) 
+    legend(2012,8,c("Real government spending","Real GDP"), 
+           col=c('firebrick3','aquamarine4'),pch=15,ncol=1,bty ="n",cex=1.1)
+    
+    if (input$hor) {
+      abline(h=0)  
+    } 
+  },height = 500, width = 600)
+  
+}
+
+shinyApp(ui = ui, server = server)
