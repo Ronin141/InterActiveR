9182f646de277e3f75a66bf60e791d062d9aff13
petrambo <31902833+petrambo@users.noreply.github.com>
2018-11-20 13:30:16 +0700

    Plot line graph and tab page

--- new_line.R
+++ new_line.R
@@ -1,8 +1,8 @@
 library(shiny)
-library(datasets)
-library(readr)
-dat <- read.csv("~/InterActiveR/line_data.csv")
-years <- dat$years
+
+dat <- read.csv("line_data.csv")
+datall <- read.csv("statistic_data.csv")
+year <- dat$years
 
 ui <- fluidPage(
   
@@ -13,7 +13,8 @@ ui <- fluidPage(
       
       radioButtons("radio", label = h3("Select the sample of countries"),
                    choices = list("EastAsia", "Europe",
-                                  "TheAmericas", "SouthAsia","Oceania","MiddleEast","Africa"),selected = "World"),
+                                  "TheAmericas","SouthAsia","Oceania","MiddleEast","Africa"),selected = "World"),
+      
       
       selectInput("vertical","Show vertical line in year(s):", 
                   choices = unique(dat$years),multiple=TRUE
@@ -26,11 +27,17 @@ ui <- fluidPage(
     mainPanel(
       #    h2("main panel"),    
       #    textOutput("text1"),
-      plotOutput("lineChart")
-    )   
+      tabsetPanel(type = "tabs",
+                  tabPanel("Plot", plotOutput("lineChart")),
+                  tabPanel("Summary", verbatimTextOutput("summary")),
+                  tabPanel("Table", tableOutput("table")),
+                  tabPanel("TableAll", tableOutput("tableall"))
+                  
+      )
+      )   
   )      
-)
 
+)
 server <- function(input, output) {
   
   #  output$text1 <- renderText({ 
@@ -43,39 +50,50 @@ server <- function(input, output) {
                         "Europe" = list(dat$gr_Europe,dat$re_Europe),
                         "TheAmericas" = list(dat$gr_TheAmericas,dat$re_TheAmericas),
                         "SouthAsia" = list(dat$gr_SouthAsia,dat$re_SouthAsia) ,
-                        "Oceania" = list(dat$gr_Oceania,dat$re_Oceania) ,
+                        "Oceania" = list(dat$gr_Oceania,dat$re_Oceania),
                         "MiddleEast" = list(dat$gr_MiddleEast,dat$re_MiddleEast) ,
                         "Africa" = list(dat$gr_Africa,dat$re_Africa)
-                        
     )  
     
     chartTitle <- switch(input$radio,
-                         "EastAsia" = "East Asia",
+                         "EastAsia" = "EastAsia",
                          "Europe" = "Europe",
                          "TheAmericas" = "TheAmericas",
                          "SouthAsia" = "SouthAsia",
                          "Oceania"="Oceania",
                          "MiddleEast" = "MiddleEast",
-                         "Africa"="Africa"
+                         "Africa"="Africa" 
     )
+   
+      
     
-    yrange <- c(1:10000)
-    xrange <- range(years)
+    yrange <- c(0,200000)
+    xrange <- range(year)
     plot(xrange,yrange,type="n",xlab="",ylab="Growth rate (percent)",cex.lab=1.5,
          main=paste("GDP-weighted averages shown for", chartTitle),
          sub=c("Data: IMF WEO (10/2015). Chart J. Zilinsky \n Note: Data for 2016 are IMF projections"))
-    lines(years,chartData[[1]],col="aquamarine4",lwd=3)
-    lines(years[1:2],na.omit(chartData[[2]]),col="firebrick3",lwd=3)
+    lines(year,chartData[[1]],col="aquamarine4",lwd=3)
+    lines(year[1:6],na.omit(chartData[[2]]),col="firebrick3",lwd=3)
     abline(v=input$vertical,lty=2) 
-    legend(2016,8,c("Real government spending","Real GDP"), 
+    legend(2016,50000,c("Real government spending","Real GDP"), 
            col=c('firebrick3','aquamarine4'),pch=15,ncol=1,bty ="n",cex=1.1)
     
     if (input$hor) {
       abline(h=0)  
     } 
-  },height = 500, width = 600)
+  },height = 700, width = 800)
   
+  output$summary <- renderPrint({
+    ee <- dat
+    summary(ee)
+  })
+  
+  output$table <- renderTable({
+    dat
+  })
+  output$tableall <- renderTable({
+    datall
+  })
 }
 
 shinyApp(ui = ui, server = server)
-
--- test.r
+++ test.r
@@ -1,2 +1,31 @@
-raw_data = read.csv("raw_data.csv")
-print(raw_data)
+library(shiny)
+require(shiny)
+library(datasets)
+
+shinyUI(
+  
+  # Use a fluid Bootstrap layout
+  fluidPage(    
+    
+    # Give the page a title
+    titlePanel("Telephones Around the World"),
+    
+    # Generate a row with a sidebar
+    sidebarLayout(      
+      
+      # Define the sidebar with one input
+      sidebarPanel(
+        selectInput("region", "Region:", 
+                    choices=colnames(stat)),
+        hr(),
+        helpText("Data from AT&T (1961) The World's Telephones.")
+      ),
+      
+      # Create a spot for the barplot
+      mainPanel(
+        plotOutput("phonePlot")  
+      )
+      
+    )
+  )
+)
